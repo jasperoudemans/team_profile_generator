9245e7caf77af66273ec0c0b9bbc2892ad9471ee
Jasper Oudemans <jasperoudemans@gmail.com>
2022-01-22 12:56:10 -0700

    added test for createIntern

---
+++ createIntern.test.js
@@ -0,0 +1,20 @@
+const { intern_questions } = require("./constants.js");
+const createIntern = require("./createIntern.js");
+
+it("Creates an intern with provided info", () => {
+  const sampleIntern = {
+    name: "Ringo",
+    id: "03",
+    email: "ringo@mail.com",
+    school: "School of Beatles",
+  };
+
+  const ringo = createIntern(sampleIntern);
+  expect(ringo).toBe(`<div class="card">
+    <h3>Intern</h3>
+    <p>Name: Ringo</p>
+    <p>ID: 03</p>
+    <p>Email: ringo@mail.com</p>
+    <p>School: School of Beatles</p>
+  </div>`);
+});
